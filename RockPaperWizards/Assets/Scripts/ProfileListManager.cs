90f2d790ced2c321865a5bcc6a0baeabbf1d28c0
luisgvecchio <90183135+luisgvecchio@users.noreply.github.com>
2022-01-27 17:33:42 +0100

    Now it saves on .json files

--- RockPaperWizards/Assets/Scripts/ProfileListManager.cs
+++ RockPaperWizards/Assets/Scripts/ProfileListManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using System.IO;
+using System.Text;
 
 public class ProfileListManager : MonoBehaviour
 {
@@ -25,6 +27,8 @@ public class ProfileListManager : MonoBehaviour
         profilesList.Profiles.Add(inputFieldP2.text);
     }
 
+
+    //It is saving on both PlayerPrefs and ProfilesData.json
     public void UpdateProfileList()
     {
         ProfilesSaveData profilesSave = new ProfilesSaveData();
@@ -33,17 +37,53 @@ public class ProfileListManager : MonoBehaviour
 
         string jsonString = JsonUtility.ToJson(profilesSave);
         PlayerPrefs.SetString("SavedProfilesList", jsonString);
+
+        SaveToFile("ProfilesData.json", jsonString);
     }
+
+    //It only loads from ProfileData.json
     public void LoadProfileList()
     {
-        string jsonString = PlayerPrefs.GetString("SavedProfilesList");
+        //string jsonString = PlayerPrefs.GetString("SavedProfilesList");
 
-        profilesList = JsonUtility.FromJson<ProfilesSaveData>(jsonString);
+        profilesList = JsonUtility.FromJson<ProfilesSaveData>(LoadFromFile("ProfilesData.json"));
     }
 
     public void EraseAllListElements()
     {
         profilesList.Profiles.Clear();
+    }
+
+    public void SaveToFile(string fileName, string jsonString)
+    {
+        // Open a file in write mode. This will create the file if it's missing.
+        // It is assumed that the path already exists.
+        using (var stream = File.OpenWrite(fileName))
+        {
+            // Truncate the file if it exists (we want to overwrite the file)
+            stream.SetLength(0);
+
+            // Convert the string into bytes. Assume that the character-encoding is UTF8.
+            // Do you not know what encoding you have? Then you have UTF-8
+            var bytes = Encoding.UTF8.GetBytes(jsonString);
+
+            // Write the bytes to the hard-drive
+            stream.Write(bytes, 0, bytes.Length);
+
+            // The "using" statement will automatically close the stream after we leave
+            // the scope - this is VERY important
+            Debug.Log("working");
+        }
+    }
+    public string LoadFromFile(string fileName)
+    {
+        // Open a stream for the supplied file name as a text file
+        using (var stream = File.OpenText(fileName))
+        {
+            // Read the entire file and return the result. This assumes that we've written the
+            // file in UTF-8
+            return stream.ReadToEnd();
+        }
     }
 
 }
